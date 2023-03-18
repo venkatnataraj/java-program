39cd3a44fa2762637ac1166fded2a305059d7e79
venkatnataraj <128207602+venkatnataraj@users.noreply.github.com>
2023-03-18 12:50:31 +0530

    Update string_reverse.java

--- string_reverse.java
+++ string_reverse.java
@@ -1,16 +1,14 @@
 // Reverse a String 
-// getting input string from user and print the reverse string
+
 
 
 package practice;
-import java.util.Scanner;
 public class string_reverse {
-    static Scanner s=new Scanner(System.in);
+    
 	public static void main(String[] args) {
-		System.out.println("Enter the name for reverse "); // getting input from user..  
-		String name=""; 
-		name=s.next();
-        String reverse="";
+		 
+		String name="Dhoni"; 
+		String reverse="";
     
         for(int i=name.length()-1;i>=0;i--) {
         	reverse+=name.charAt(i);
